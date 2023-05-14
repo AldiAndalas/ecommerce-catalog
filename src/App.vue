677d811985e9620ce7ec3f4cda654e61e466b4a2
unknown <aldiandalas@hotmail.com>
2023-05-14 23:02:28 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket</h1>
+  <img src="images 1.png" alt="">
+</template>
+
+<script>
+</script>
+
+<style>
+ .h1 {
+  position: absolute;
+width: 548px;
+height: 68px;
+left: 577px;
+top: 176px;
+background-color: #FDE2FF;
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 28px;
+line-height: 34px;
+
+color: #720060;
+}
+
+
+</style>
